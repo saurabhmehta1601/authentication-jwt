e9fce390b11806449c911d379a09eea54ca5b8af
Saurabh Mehta <saurabhemehta1601@gmail.com>
2021-05-13 14:27:42 +0530

    add mongoose schema

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,17 +1,19 @@
 import {Request, Response, Router} from "express"
-
 import {Document} from "mongoose"
 
 interface IUser extends Document {
-
+    name:string,
+    email :string,
+    password : string,
 }
 
 
 const router = Router()
 router.route("/register").post( async (req: Request,res : Response)=>{
    const {email,password } =req.body 
-    
+
    try{
+    //    const user : IUser  =await 
    }catch(err){
     console.info("user registration failed ", err)
    }
